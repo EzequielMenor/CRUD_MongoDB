735e6f9c09cc42f9f7450a29c88da8c187d9428f
Ezequiel Menor <ezequielmenor@gmail.com>
2024-04-15 16:23:10 +0200

    borrar y editar temas

--- app/addTopic/page.jsx
+++ app/addTopic/page.jsx
@@ -27,20 +27,22 @@ export default function AddTopic() {
       });
 
       if (res.ok) {
-        
+        router.push('/');
+      } else {
+        throw new Error("Fallo al crear el tema");
       }
     } catch (error) {
-
+      console.log(error);
     }
   }
 
   return (
     <form onSubmit={handleSubmit} className="flex flex-col gap-3">
-      <input onChange={(e) => setTitle(e.target.value)} value={title} className="border border-slate-500 px-8 py-2" type="text" placeholder="Topic Title" />
-      <input onChange={(e) => setDescription(e.target.value)} value={description} className="border border-slate-500 px-8 py-2" type="text" placeholder="Topic Description" />
+      <input onChange={(e) => setTitle(e.target.value)} value={title} className="border border-slate-500 px-8 py-2" type="text" placeholder="Titulo del Tema" />
+      <input onChange={(e) => setDescription(e.target.value)} value={description} className="border border-slate-500 px-8 py-2" type="text" placeholder="Descripción del tema" />
 
       <button type="submit" className="bg-green-600 font-bold text-white py-3 px-6 w-fit">
-        Add Topic
+        Añadir Tema
       </button>
     </form>
   )
