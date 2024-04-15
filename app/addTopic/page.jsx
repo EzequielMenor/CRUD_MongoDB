014ad33d61cea43835333c05c6531f03fa897eee
Ezequiel Menor <ezequielmenor@gmail.com>
2024-04-15 13:07:34 +0200

    editar topics

--- app/addTopic/page.jsx
+++ app/addTopic/page.jsx
@@ -1,10 +1,45 @@
-export default function AddTopic() {
-  return (
-    <form className="flex flex-col gap-3">
-      <input className="border border-slate-500 px-8 py-2" type="text" placeholder="Topic Title" />
-      <input className="border border-slate-500 px-8 py-2" type="text" placeholder="Topic Description" />
+"use client";
 
-      <button className="bg-green-600 font-bold text-white py-3 px-6 w-fit">
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+export default function AddTopic() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!title || !description) {
+      alert('El título y la descripción son obligatorios.');
+      return;
+    }
+
+    try {
+      const res = await fetch('http://localhost:3000/api/topics', {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json"
+        },
+        body: JSON.stringify({ title, description })
+      });
+
+      if (res.ok) {
+        
+      }
+    } catch (error) {
+
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="flex flex-col gap-3">
+      <input onChange={(e) => setTitle(e.target.value)} value={title} className="border border-slate-500 px-8 py-2" type="text" placeholder="Topic Title" />
+      <input onChange={(e) => setDescription(e.target.value)} value={description} className="border border-slate-500 px-8 py-2" type="text" placeholder="Topic Description" />
+
+      <button type="submit" className="bg-green-600 font-bold text-white py-3 px-6 w-fit">
         Add Topic
       </button>
     </form>
